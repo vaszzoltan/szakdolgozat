d1095bc885b8f5433ac12eefc3a00137b45d5098
Vasz Zoltan <vaszzoltan@gmail.com>
2022-06-24 10:21:09 +0200

    initial commit

---
+++ src/main/java/com/vaszily/WorkoutPlanner/WorkoutPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.vaszily.WorkoutPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaszily/WorkoutPlanner/WorkoutPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaszily.WorkoutPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
