44d47f08f02272cd91a36788e8f9eafca6b33069
Vasz Zoltan <vaszzoltan@gmail.com>
2022-08-13 10:01:44 +0200

    add findAllByName meth to task repo

--- src/main/java/com/vaszily/WorkoutPlanner/repositories/TaskRepo.java
+++ src/main/java/com/vaszily/WorkoutPlanner/repositories/TaskRepo.java
@@ -4,6 +4,9 @@ import com.vaszily.WorkoutPlanner.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepo extends JpaRepository<Task, Long> {
+    List<Task> findAllByName(String name);
 }
