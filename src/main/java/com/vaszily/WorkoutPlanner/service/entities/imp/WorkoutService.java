7045a5886585b99ed0bf611e3d2f330c1454f6c0
Vasz Zoltan <vaszzoltan@gmail.com>
2022-08-13 10:22:03 +0200

    workout service basic impl

--- src/main/java/com/vaszily/WorkoutPlanner/service/entities/imp/WorkoutService.java
+++ src/main/java/com/vaszily/WorkoutPlanner/service/entities/imp/WorkoutService.java
@@ -1,39 +1,59 @@
 package com.vaszily.WorkoutPlanner.service.entities.imp;
 
 import com.vaszily.WorkoutPlanner.model.Workout;
+import com.vaszily.WorkoutPlanner.repositories.WorkoutRepo;
 import com.vaszily.WorkoutPlanner.service.entities.EntityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 @Service
 public class WorkoutService implements EntityService<Workout> {
+    private final WorkoutRepo workoutRepo;
+    @Autowired
+    public WorkoutService(WorkoutRepo workoutRepo) {
+        this.workoutRepo = workoutRepo;
+    }
+
     @Override
     public List<Workout> getAll() {
-        return null;
+        return workoutRepo.findAll();
     }
 
     @Override
     public List<Workout> getAllByName(String name) {
-        return null;
+        return workoutRepo.findAllByName(name);
     }
 
     @Override
     public Workout getById(Long Id) {
-        return null;
+        return workoutRepo.findById(Id).orElseThrow(EntityNotFoundException::new);
     }
 
     @Override
     public Workout save(Workout toSave) {
-        return null;
+        return workoutRepo.save(toSave);
     }
 
     @Override
-    public Workout update(Long id, Workout toUpdate) {
-        return null;
+    public Workout update(Long id, Workout workout) {
+        Workout toUpdate = workoutRepo.findById(id).orElseThrow(EntityNotFoundException::new);
+        toUpdate.setName(workout.getName());
+        toUpdate.setAccounts(workout.getAccounts());
+        toUpdate.setAfterEffect(workout.getAfterEffect());
+        toUpdate.setDescription(workout.getDescription());
+        toUpdate.setFinishDate(workout.getFinishDate());
+        toUpdate.setGoalDate(workout.getGoalDate());
+        toUpdate.setTasks(workout.getTasks());
+        toUpdate.setWorkoutPlans(workout.getWorkoutPlans());
+        return workoutRepo.save(toUpdate);
     }
 
     @Override
     public void delete(Long id) {
+        Workout toDelete = workoutRepo.findById(id).orElseThrow(EntityNotFoundException::new);
+        workoutRepo.delete(toDelete);
 
     }
 }
