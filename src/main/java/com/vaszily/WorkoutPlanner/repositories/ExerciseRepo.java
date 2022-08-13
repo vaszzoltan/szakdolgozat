657005942f4fdaeb0577557a5edee6bde76aacdb
Vasz Zoltan <vaszzoltan@gmail.com>
2022-08-13 09:48:47 +0200

    add query findAllByName

--- src/main/java/com/vaszily/WorkoutPlanner/repositories/ExerciseRepo.java
+++ src/main/java/com/vaszily/WorkoutPlanner/repositories/ExerciseRepo.java
@@ -4,6 +4,9 @@ import com.vaszily.WorkoutPlanner.model.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ExerciseRepo extends JpaRepository<Exercise, Long> {
+    List<Exercise> findAllByName(String name);
 }
