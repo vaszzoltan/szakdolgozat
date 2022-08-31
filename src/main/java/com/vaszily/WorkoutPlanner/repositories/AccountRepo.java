b5e7e5c881ff60c76c90eb9d45d1c6e75660f703
Rasyn <rasyn@Rasyn.Rasyn>
2022-08-31 11:46:51 +0200

    implement account service, add findAllByName to repo

--- src/main/java/com/vaszily/WorkoutPlanner/repositories/AccountRepo.java
+++ src/main/java/com/vaszily/WorkoutPlanner/repositories/AccountRepo.java
@@ -4,6 +4,9 @@ import com.vaszily.WorkoutPlanner.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AccountRepo extends JpaRepository<Account, Long> {
+    List<Account> findAllByName(String name);
 }
--- src/main/java/com/vaszily/WorkoutPlanner/service/entities/imp/AccountService.java
+++ src/main/java/com/vaszily/WorkoutPlanner/service/entities/imp/AccountService.java
@@ -1,40 +1,52 @@
 package com.vaszily.WorkoutPlanner.service.entities.imp;
 
 import com.vaszily.WorkoutPlanner.model.Account;
+import com.vaszily.WorkoutPlanner.repositories.AccountRepo;
 import com.vaszily.WorkoutPlanner.service.entities.EntityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityExistsException;
 import java.util.List;
 
 @Service
 public class AccountService implements EntityService<Account> {
+    AccountRepo accountRepo;
+    @Autowired
+    public AccountService(AccountRepo accountRepo){
+        this.accountRepo = accountRepo;
+    }
     @Override
     public List<Account> getAll() {
-        return null;
+        return accountRepo.findAll();
     }
 
     @Override
     public List<Account> getAllByName(String name) {
-        return null;
+        return accountRepo.findAllByName(name);
     }
 
     @Override
     public Account getById(Long Id) {
-        return null;
+        return accountRepo.findById(Id).orElseThrow(EntityExistsException::new);
     }
 
     @Override
     public Account save(Account toSave) {
-        return null;
+        return accountRepo.save(toSave);
     }
 
     @Override
     public Account update(Long id, Account toUpdate) {
-        return null;
+        Account account  = getById(id);
+        account.setName(toUpdate.getName());
+        account.setWorkouts(toUpdate.getWorkouts());
+        account.setWorkoutPlans(toUpdate.getWorkoutPlans());
+        return account;
     }
 
     @Override
     public void delete(Long id) {
-
+        accountRepo.delete(getById(id));
     }
 }
