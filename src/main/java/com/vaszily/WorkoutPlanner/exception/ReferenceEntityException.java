69724345b1f0314c3cea29b321fb99e6673ee565
Zoltán Vaszily <vaszzoltan@gmail.com>
2022-09-20 14:09:55 +0200

    create custom runtime exception

---
+++ src/main/java/com/vaszily/WorkoutPlanner/exception/ReferenceEntityException.java
@@ -0,0 +1,7 @@
+package com.vaszily.WorkoutPlanner.exception;
+
+public class ReferenceEntityException extends RuntimeException{
+    public ReferenceEntityException(String msg){
+        super(msg);
+    }
+}
