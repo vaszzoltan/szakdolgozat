e1823b89c3519416b9ac4a32e4decae93780f206
Zoltán Vaszily <vaszzoltan@gmail.com>
2022-10-05 15:35:28 +0200

    create custom exception

---
+++ src/main/java/com/vaszily/WorkoutPlanner/exception/DataUploadException.java
@@ -0,0 +1,7 @@
+package com.vaszily.WorkoutPlanner.exception;
+
+public class DataUploadException extends RuntimeException{
+    public DataUploadException(String msg){
+        super(msg);
+    }
+}
